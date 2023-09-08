dd7b9b685af2628ffb2d22ecbf0648f73d4045fd
8825841493 <144448709+8825841493@users.noreply.github.com>
2023-09-08 17:20:57 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+#1.1implement a recursive function to calculate the factorial of a given number. 
+n=int(input("Enter number:"))
+fact=1
+while(n>0):
+    fact=fact*n
+    n=n-1
+print("Factorial of the number is: ")
+print(fact)
